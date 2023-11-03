3ee405dbfe9e55dd2458675b7f32cd2b2a6c6f12
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-11-03 12:00:05 +0530

    Update totalPopulation.js

--- totalPopulation.js
+++ totalPopulation.js
@@ -1,1 +1,20 @@
+/*Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
+Print the total population of countries using reduce function*/
+
+const XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(XHR.responseText);
+
+    const totalPopulation = data.reduce((total, country) => {
+      return total + country.population;
+    }, 0);
+
+    console.log('Total Population of Countries:', totalPopulation);
+  }
+};
+
+XHR.open('GET', 'https://restcountries.com/v3.1/all');
+XHR.send();
 
