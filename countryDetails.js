dd1a2dccce4c9f78b76a8303bec4f67ac1627acc
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-11-03 11:52:30 +0530

    Create countryDetails.js

---
+++ countryDetails.js
@@ -0,0 +1,28 @@
+/*Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
+  c. Print the following details name, capital, flag, using forEach function*/
+
+const XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(XHR.responseText);
+
+    data.forEach((country) => {
+      console.log('Name:', country.name.common);
+      if (country.capital && country.capital[0]) {
+        console.log('Capital:', country.capital[0]);
+      } else {
+        console.log('Capital: N/A');
+      }
+      if (country.flag) {
+        console.log('Flag:', country.flag);
+      } else {
+        console.log('Flag: N/A');
+      }
+      console.log('---'); // Separation between countries
+    });
+  }
+};
+
+XHR.open('GET', 'https://restcountries.com/v3.1/all');
+XHR.send();
