5083de36c92cf6f8b114e9f89cb00783e8064329
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-11-03 12:33:38 +0530

    Update PopulationLessThan2L.js

--- PopulationLessThan2L.js
+++ PopulationLessThan2L.js
@@ -1,3 +1,4 @@
+//Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 //b. Get all the countries with a population of less than 2 lakhs using Filter function
 
 const XHR = new XMLHttpRequest();
