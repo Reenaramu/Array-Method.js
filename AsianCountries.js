ab8f3b42681be00341bb078b6d00e6c0661c1cb3
Reenaramu <142896131+Reenaramu@users.noreply.github.com>
2023-11-03 11:07:22 +0530

    Update AsianCountries.js

--- AsianCountries.js
+++ AsianCountries.js
@@ -1,4 +1,20 @@
 /*Solving problems using array functions on rest countries data (https://restcountries.com/v3.1/all).
 a. Get all the countries from Asia continent /region using Filter function*/
 
+const XHR = new XMLHttpRequest();
+
+XHR.onreadystatechange = function () {
+  if (this.readyState === 4 && this.status === 200) {
+    const data = JSON.parse(XHR.responseText);
+
+    const asiaCountryNames = data
+      .filter((country) => country.continents.indexOf("Asia") !== -1)
+      .map((country) => country.name.common);
+
+    console.log('Asian Countries:', asiaCountryNames);
+  }
+};
+
+XHR.open('GET', 'https://restcountries.com/v3.1/all');
+XHR.send();
 
